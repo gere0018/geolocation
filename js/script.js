0247f7e5b01f780c8835c77d8bbfb63fa3f4f420
Nehmat <gere0018@algonquinlive.com>
2015-01-26 11:19:19 -0500

    map showing, marker not yet

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function(){
+
   if( navigator.geolocation ){
-    var getLocation = {enableHighAccuracy: false, timeout:3600, maximumAge:3600};
+    var getLocation = {enableHighAccuracy: false, timeout:60000, maximumAge:60000};
     navigator.geolocation.getCurrentPosition( reportPosition, gpsError, getLocation );
-    drawImage();
-      alert("hello");
   }else{
     alert("OOPS!! your browser needs to be updated and currently does not support location based services.")
   }
@@ -14,21 +13,22 @@ function reportPosition( position ){
   var output = document.querySelector("#output");
   output.innerHTML += "Latitude: " + position.coords.latitude + "&deg;<br/>"
   + "Longitude: " + position.coords.longitude + "&deg;<br/>"
-  + "Altitude: " + position.coords.altitude + " m<br/>"
+drawImage(position);
 }
-function drawImage(){
+
+function drawImage(position){
   var canvas = document.createElement("canvas");
   var context = canvas.getContext('2d');
   var x = 0;
   var y = 0;
-  var width = 400px;
-  var height = 400px;
+  var width = 400;
+  var height = 400;
   var imageObj = new Image();
  imageObj.onload = function() {
     context.drawImage(imageObj, x, y, width, height);
   };
- imageObj.src = 'https://maps.googleapis.com/maps/api/staticmap?center=Banf,Alberta&amp;zoom=14&size=400x400&amp;markers=size:mid%7Ccolor:red%7Ccenter;key=AIzaSyAOXUJlnbNxvV7HRtrslv_vFBT7tCJ4VZc';
-
+ imageObj.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + ","+ position.coords.longitude + "&zoom=14&size=400x400&markers=color:red%7C" + position.coords.latitude + ","+ position.coords.longitude + ";key=AIzaSyAOXUJlnbNxvV7HRtrslv_vFBT7tCJ4VZc";
+document.querySelector('#output').appendChild(canvas);
 }
 
 function gpsError( error ){

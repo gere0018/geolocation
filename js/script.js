ed22661b03b7423424abaa6c21baa4aa3d1c5c94
Nehmat <gere0018@algonquinlive.com>
2015-01-26 09:38:13 -0500

    created canvas element and image, yet to get a result on a browser

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
 
+document.addEventListener("DOMContentLoaded", function(){
   if( navigator.geolocation ){
-    var getLocation = {enableHighAccuracy: false, timeout:3600, maximumAge:0};
+    var getLocation = {enableHighAccuracy: false, timeout:3600, maximumAge:3600};
     navigator.geolocation.getCurrentPosition( reportPosition, gpsError, getLocation );
+    drawImage();
+      alert("hello");
   }else{
     alert("OOPS!! your browser needs to be updated and currently does not support location based services.")
   }
@@ -12,8 +14,21 @@ function reportPosition( position ){
   var output = document.querySelector("#output");
   output.innerHTML += "Latitude: " + position.coords.latitude + "&deg;<br/>"
   + "Longitude: " + position.coords.longitude + "&deg;<br/>"
-  + "Accuracy: " + position.coords.accuracy + "m<br/>"
   + "Altitude: " + position.coords.altitude + " m<br/>"
+}
+function drawImage(){
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext('2d');
+  var x = 0;
+  var y = 0;
+  var width = 400px;
+  var height = 400px;
+  var imageObj = new Image();
+ imageObj.onload = function() {
+    context.drawImage(imageObj, x, y, width, height);
+  };
+ imageObj.src = 'https://maps.googleapis.com/maps/api/staticmap?center=Banf,Alberta&amp;zoom=14&size=400x400&amp;markers=size:mid%7Ccolor:red%7Ccenter;key=AIzaSyAOXUJlnbNxvV7HRtrslv_vFBT7tCJ4VZc';
+
 }
 
 function gpsError( error ){
